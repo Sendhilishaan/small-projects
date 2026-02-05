8af2664126eb9317a1c7d0c80dfa9abe70bfb18b
Sendhilishaan <ishaansendhil@gmail.com>
2026-02-05 00:00:30 -0500

    complete implementation

--- sieve-of-eratosthenes/soe.c
+++ sieve-of-eratosthenes/soe.c
@@ -40,17 +40,42 @@ int main(int argc, char* argv[]) {
 
 	primes.nums = calloc(approx_cap, sizeof(int));
 	primes.capacity = approx_cap;
-	primes.count = 0;
+	primes.nums[0] = 2;
+	primes.count = 1;
 
 	// 3 to n, not including even numbers
 	numbers.nums = malloc(sizeof(int) * ((n+1) / 2) - 1);
-	numbers.count = (n+1) / 2;
+	numbers.capacity = (n+1) / 2;
 	numbers.count = 0;
 
 	// sieve algorithm
-	for (int p = 3; p < n - (n % p); p += 2) {
+	for (int p = 3; p <= n;) {
+
+		primes.nums[primes.count] = p;
+		primes.count++;
 		
+		for (int multiple = 3 * p; multiple <= n; multiple += 2 * p) {
+			numbers.nums[(multiple - 3) / 2] = marked;
+		}
+
+		int p_check = p;
+
+		for (int i = 2; p < n; i += 2) {
+			if (numbers.nums[(p + i - 3) / 2] == unmarked) {
+				p = p + i;
+				break;
+			}
+		}
+
+		if (p_check == p) { break; }
+
 	}
+	for (int x = 0; x < primes.count; x++) {
+		printf("%d, ", primes.nums[x]);
+	}
+
+	free(numbers.nums);
+	free(primes.nums);
 
 	return 1;
 }
