1bda175a6f902c3c3d830e13b52bd63bb82cafb4
Sendhilishaan <ishaansendhil@gmail.com>
2026-02-26 15:16:47 -0500

    boiler and desgin

--- sand-sim/sand.cpp
+++ sand-sim/sand.cpp
@@ -1,63 +1,61 @@
-#include "SDL2/SDL.h"
-#include "SDL2/SDL_surface.h"
-#include "SDL2/SDL_video.h"
-#include <arm_neon.h>
-#include <cstddef>
-#include <cstdint>
-#include <iostream>
-#include <stdint.h>
+#include <stdio.h>
+#include "SDL2/SDL_events.h"
+#include "SDL2/SDL_keycode.h"
+#include "SDL2/SDL_mouse.h"
+#include <SDL2/SDL_rect.h>
+#include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_video.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <string.h>
 #include <vector>
 
 #define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
+#define	SCREEN_HEIGHT 480
 #define PIXEL_SIZE 5
+#define FREE 0
+#define OCCUPIED 1
 
 using namespace std;
 
-class Particle {
-	public:
-		
-		uint32_t x, y;
-		uint8_t color;
-		
-		Particle(): x(0), y(0), color(0) {}
+typedef struct {
+	int x, y;
+	bool is_moving;
+	SDL_Rect rect;
 
-		Particle(uint64_t xCord, uint64_t yCord): x(xCord), y(yCord) {}
+} Sand;
 
-		virtual void tick() = 0;
+array<array<int, SCREEN_HEIGHT>, SCREEN_WIDTH> grid = {0};
+vector<Sand> sand_list;
 
-};
-
-class Sand: public Particle {
-
-	public:
-
-		Sand(uint64_t x, uint64_t y) : Particle(x, y) {
-			color = 1;
+void move_all() {
+	for (Sand& grain : sand_list) {
+		if (grain.y - 1 > SCREEN_HEIGHT / PIXEL_SIZE) { 
+			if (grid[grain.x][grain.y - 1] == FREE) {
+				grain.y -= 1;
+			}
+			
 		}
-	
-		virtual void tick() {
-			x -= 1;
-			y -= 1;
-		}
-};
+	}
+}
 
-vector<vector<Particle>> grid(SCREEN_WIDTH, vector<Particle>(SCREEN_HEIGHT));
-vector<vector<uint64_t> > liveParticles;
+void sort_sand() {
+	// keep the sand in the sand_list sorted in non-decreasing y values
+}
 
 int main() {
-	cout << "hi";
-	
 	SDL_Window *window = NULL;
 	SDL_Surface *screenSurface = NULL;
-
+	
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		printf("Failed to init SDL");
 		exit(1);
 	}
 
 	window = SDL_CreateWindow(
-			"cgol",
+			"sand-simulation!",
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
 		SCREEN_WIDTH,
@@ -71,19 +69,41 @@ int main() {
 	}
 	
 	SDL_Event event;
-	uint8_t running = 1;
+	int running = 1;
 	SDL_Surface *surface = SDL_GetWindowSurface(window);
-
-	SDL_UpdateWindowSurface(window);
 
 	while (running) {
 		while(SDL_PollEvent(&event)) {
 			if (event.type == SDL_QUIT) {
-				SDL_Quit();
 				running = 0;
 			}
+
+			if (event.button.button == SDL_BUTTON_LEFT && event.type == SDL_MOUSEBUTTONDOWN && grid[event.button.x][event.button.y] == 1) {
+				int x = event.button.x / PIXEL_SIZE;
+				int y = event.button.y / PIXEL_SIZE;
+
+				Sand newcell = {
+					x,
+					y,
+					true,
+					SDL_Rect{x, y, PIXEL_SIZE, PIXEL_SIZE}
+				};
+
+				grid[x][y] = 1;
+				sand_list.push_back(newcell);
+			}
+
+			// sdl events here, once per frame
+
 		}
+
+		// main logic here
+
+		SDL_UpdateWindowSurface(window);
+		SDL_Delay(16); //framerate
+
 	}
 
-	return 0;
+	return 1;
 }
+
